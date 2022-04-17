364559d940c54eb005cfb76ffda1660a5541d9ff
yjqian02 <yjqian02@gmail.com>
2022-04-17 15:53:34 -0500

    working on last step

--- tabulation.py
+++ tabulation.py
@@ -7,12 +7,12 @@
 #     Total number of unique authors
 #     Average post length (measured in word count)
 #     Date range of the dataset
-#     Top 20 most important words in the posts (selftext
+#     Top 20 most important words in the posts
 
 import pandas
 import re
 from datetime import datetime
-import time
+from gensim.summarization import keywords
 
 def tabulate(filename):
     data = pandas.read_csv(filename, header=0)
@@ -27,33 +27,34 @@ def tabulate(filename):
     num_removed = len(data_removed)
     posts_clean = data["selftext"].str.replace(r'[^\w\s]', '', flags=re.UNICODE) 
     # reference: https://stackoverflow.com/questions/47464658/python-efficient-way-to-remove-emojis-and-some-punctuation-from-a-large-dataset
+    
+    # avg length of posts
+    posts_txt = ""
     sum = 0
     for i in range(len(posts_clean)):
         post = str(posts_clean[i])
         post = post.split(" ")
         if post != "[deleted]" and post != "[removed]":
             sum += len(post)
+            posts_txt += post
     
+    # date range
     dates = data["created_utc"]
     dt_dates = []
-    # print(dates[16052])
     for i in range(len(dates)):
-        if "http" not in dates[i]:
-        # d = int(dates[i])
-        # print(d)
+        if "http" not in dates[i]: # skip over invalid dates/erroring data
             dt_dates.append(datetime.fromtimestamp(int(dates[i])))
     dt_dates.sort()
-    # x = datetime.fromtimestamp(int(data["created_utc"][0]))
-    # y = datetime.fromtimestamp(int(data["created_utc"][2]))
-    # print(x > y, x, y)
-    print("The date range of the datset is from {} to {}.\n".format(dt_dates[0], dt_dates[-1]))
+
+    # 20 most important posts
+    print(keywords(posts_txt))
     
     # print("There are", num_posts, "posts in the dataset.\n")
     # print("There are", num_deleted, "deleted posts in the dataset.\n")
     # print("There are", num_removed, "removed posts in the dataset.\n")
     # print("There are", len(unique_authors), "unique authors in the dataset.\n")
     # print("The average post length is {:0.2f} words.\n".format(sum / (num_posts - num_deleted - num_removed)))
-
+    # print("The date range of the datset is:\n{} to {}.\n".format(dt_dates[0], dt_dates[-1]))
 
 if __name__ == "__main__":
     tabulate('depression-sampled.csv')
